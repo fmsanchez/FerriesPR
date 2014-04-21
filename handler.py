1c449f8292bb1e37852436ed2d90bea74ef520f1
Francisco Sanchez <fmsanchez001@gmail.com>
2014-04-20 22:07:13 -0400

    Added server files and updated payment forms

---
+++ handler.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import stripe
+import cgi
+
+form = cgi.FieldStorage()
+# Set your secret key: remember to change this to your live secret key in production
+# See your keys here https://manage.stripe.com/account
+stripe.api_key = "sk_test_RZkRJdFi3icYHHqxjCM64UR2"
+
+# Get the credit card details submitted by the form
+token = form['stripeToken']
+
+# Create the charge on Stripe's servers - this will charge the user's card
+try:
+  charge = stripe.Charge.create(
+      amount=1000, # amount in cents, again
+      currency="usd",
+      card=token,
+      description="payinguser@example.com"
+  )
+except stripe.CardError, e:
+  # The card has been declined
+  pass
+
--- scriptMainPage.js
+++ scriptMainPage.js
@@ -154,7 +154,7 @@ $(document).ready(function () {
             }
         }
     });
-    
+
 
     $('#continueButton').click(function() {
         $('#ErrorMessage').hide()
@@ -212,9 +212,36 @@ $(document).ready(function () {
         }
     });
 
-    $('#confirmButton').click(function() {
-        $('#PaginaPago').hide();
-        $('#ConfirmPage').show();
+    var stripeResponseHandler = function(status, response) {
+        var form = $('#payment-form');
+
+          if (response.error) {
+            // Show the errors on the form
+            form.find('.payment-errors').text(response.error.message);
+            form.find('button').prop('disabled', false);
+          } else {
+            // token contains id, last4, and card type
+            var token = response.id;
+            // Insert the token into the form so it gets submitted to the server
+            form.append($('<input type="hidden" name="stripeToken" />').val(token));
+            // and submit
+            form.get(0).submit();
+        }
+    };
+
+    $('#payment-form').submit(function() {
+        var form = $('#payment-form');
+
+        // Disable the submit button to prevent repeated clicks
+        form.find('button').prop('disabled', true);
+
+        Stripe.card.createToken(form, stripeResponseHandler);
+
+        // Prevent the form from submitting with the default action
+        return false;
+       /* $('#PaginaPago').hide();
+        $('#ConfirmPage').show(); */
     });
+
 
 });
